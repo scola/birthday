d2e63d767ea64594f597f3248406820d4d3c5452
scola <shaozheng.wu@gmail.com>
2015-01-06 09:48:28 +0800

    start to sync by google api

---
+++ src/com/google/api/services/samples/calendar/android/AsyncInsertEvent.java
@@ -0,0 +1,22 @@
+package com.google.api.services.samples.calendar.android;
+
+//import com.google.api.services.calendar.model.Calendar;
+import com.google.api.services.calendar.model.Event;
+
+import java.io.IOException;
+
+public class AsyncInsertEvent extends CalendarAsyncTask {
+	private final String calendar_id;
+	private final Event event;
+	AsyncInsertEvent(CalendarSampleActivity calendarSample, String calendar_id, Event event) {
+	    super(calendarSample);
+	    this.calendar_id = calendar_id;
+	    this.event = event;
+	  }
+	
+	  @Override
+	  protected void doInBackground() throws IOException {
+		  Event createEvent = client.events().insert(calendar_id, event).execute();
+	    //model.add(event);
+	  }
+}
