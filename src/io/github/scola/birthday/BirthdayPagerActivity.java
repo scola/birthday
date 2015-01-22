a823f51beca2e1ce7de73c5838c2199f01883f4d
scola <shaozheng.wu@gmail.com>
2015-01-22 11:10:09 +0800

    rm useless files

--- src/io/github/scola/birthday/BirthdayPagerActivity.java
+++ src/io/github/scola/birthday/BirthdayPagerActivity.java
@@ -8,7 +8,6 @@ import android.os.Bundle;
 import android.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.app.FragmentManager;
-//import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v13.app.FragmentPagerAdapter;
 
 import android.support.v4.view.ViewPager;
@@ -44,13 +43,6 @@ public class BirthdayPagerActivity extends FragmentActivity {
 
             public void onPageSelected(int position) {
                 // Check if this is the page you want.
-//            	String name = birthdays.get(position).getName();
-//                if(name != null && false == name.equals(getResources().getString(R.string.summary_name_preference))){
-//                	setTitle(name + getResources().getString(R.string.event_summary));
-//                }
-//                else {
-//                	setTitle(R.string.birthdays_title);
-//                }
             	setBirthdayTitle(birthdays, position);
             }
         });
